594c3a55d25796c06936ee28865ce861e99db8e5
Thibaut Jombart <thibautjombart@gmail.com>
2017-11-24 11:48:47 +0000

    adding refs

---
+++ tests/testthat/test_hash_names.R
@@ -0,0 +1,22 @@
+context("Testing name hashing")
+
+test_that("Hashing outputs as expected", {
+    skip_on_cran()
+
+    expect_equal_to_reference(hash_names(c("sweet", "baby", "jesus")),
+                              file = "rds/hashref_1.rds")
+
+    expect_equal_to_reference(hash_names(NA),
+                              file = "rds/hashref_na.rds")
+
+    expect_equal(nrow(hash_names(NULL)), 0L)
+
+    expect_identical(hash_names("foo BaR "), hash_names("foobar"))
+
+    expect_identical(hash_names("Pétèr and Stévën"),
+                     hash_names("peter and steven"))
+
+    expect_identical(hash_names("Pïôtr and ÿgòr"),
+                     hash_names("píotr and YGÓR"))
+
+})
