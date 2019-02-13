18c8f3b1d2e45913995178c85e2cded0c2e65d20
ffinger <12323626+ffinger@users.noreply.github.com>
2019-02-13 10:39:20 +0000

    new test of empirical incubation dist

--- tests/testthat/test_estimate_incubation.R
+++ tests/testthat/test_estimate_incubation.R
@@ -22,7 +22,25 @@ exposures_two <- vector(mode = "list", length = nrow(ll))
 for (i in seq_along(exposures)) {
   exposures_two[[i]] <- sample(seq(ll$exposure_start[i], ll$exposure_end[i], by = "1 day"))
 }
-ll$boogaloo <- exposures_two 
+ll$boogaloo <- exposures_two
+
+#creating a simple line list with known incubation period dist:
+ll2 <- data.frame(
+  patient_id = 1:4,
+  onset = lubridate::as_date(c("2018-1-15", "2018-1-20", "2018-1-23", "2018-1-24")
+))
+
+ll2$exposure <- list(
+  lubridate::as_date(c("2018-1-12", "2018-1-10")),
+  lubridate::as_date(c("2018-1-12", "2018-1-14", "2018-1-17", "2018-1-19")),
+  lubridate::as_date(c("2018-1-15", "2018-1-20")),
+  lubridate::as_date(c("2018-1-23"))
+)
+
+ref_inc_period <- data.frame(
+  incubation_period = 1:8,
+  relative_frequency = c(5/16, 0, 5/16, 0, 1/8, 1/16, 0, 3/16)
+)
 
 
 test_that("an error is thrown if a data frame is not presented", {
@@ -36,14 +54,14 @@ test_that("an error is thrown if the data frame has no columns", {
 
 
 test_that("an error is thown if date_of_onset is not a Date", {
-  expect_error(empirical_incubation_dist(ll, exposure, date_of_onset), 
+  expect_error(empirical_incubation_dist(ll, exposure, date_of_onset),
               "date_of_onset must be a column of Dates. I found a column of class list")
 })
 
 
 test_that("an error is thrown if columns don't exist in the data frame", {
   # NOTE: do not edit the spacing on this.
-  expect_error(empirical_incubation_dist(ll, what, the, heck), 
+  expect_error(empirical_incubation_dist(ll, what, the, heck),
   "what is not a column in ll
   the is not a column in ll
   heck is not a column in ll")
@@ -51,8 +69,8 @@ test_that("an error is thrown if columns don't exist in the data frame", {
 
 
 test_that("an error is thrown if exposure is not a Date column when exposure_end is specified", {
-  expect_error(empirical_incubation_dist(ll, date_of_onset, exposure, exposure_end), 
-               "if exposure_end is specified, then exposure must be a vector of Dates") 
+  expect_error(empirical_incubation_dist(ll, date_of_onset, exposure, exposure_end),
+               "if exposure_end is specified, then exposure must be a vector of Dates")
 })
 
 
@@ -63,7 +81,7 @@ test_that("a warning is thrown if there are negative incubation periods", {
 
 
 test_that("empirical incubation period distribution can be calculated from start and end", {
-  dl <- empirical_incubation_dist(ll, date_of_onset, exposure_start, exposure_end) 
+  dl <- empirical_incubation_dist(ll, date_of_onset, exposure_start, exposure_end)
   dr <- empirical_incubation_dist(ll, date_of_onset, boogaloo)
   expect_identical(dr, dl)
 })
@@ -75,6 +93,14 @@ test_that("empirical incubation period distribution matches reference", {
 })
 
 
+test_that("empirical incubation period distribution matches dist reference computed by hand", {
+  skip_on_cran()
+  incubation_period_dist <- empirical_incubation_dist(ll2, onset, exposure)
+
+  expect_equal_to_reference(incubation_period_dist, ref_inc_period)
+})
+
+
 test_that("fitted gamma incubation period distribution matches reference", {
   skip_on_cran()
   fit <- fit_gamma_incubation_dist(ll, date_of_onset, exposure)
@@ -83,6 +109,6 @@ test_that("fitted gamma incubation period distribution matches reference", {
 
 
 test_that("fit_gamma_incubation_dist() rejects constant incubation periods", {
-  expect_error(fit_gamma_incubation_dist(ll, date_of_onset, constant_exposure), 
+  expect_error(fit_gamma_incubation_dist(ll, date_of_onset, constant_exposure),
                "incubation period is constant")
 })
