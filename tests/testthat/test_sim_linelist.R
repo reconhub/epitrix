1296edca5ca228245ccdb03d4f5fea78b3b0cbff
Thibaut Jombart <thibautjombart@gmail.com>
2022-12-09 10:56:21 +0100

    adding tests for sim_linelist

--- tests/testthat/test_sim_linelist.R
+++ tests/testthat/test_sim_linelist.R
@@ -1,5 +1,6 @@
 
 test_that("sim_linelist generates appropriate errors", {
+
   msg <- "n cannot be negative"
   expect_error(sim_linelist(-2), msg)
 
@@ -18,3 +19,20 @@ test_that("sim_linelist generates appropriate errors", {
 })
 
 
+
+test_that("sim_linelist generates appropriate outputs", {
+  x <- sim_linelist()
+  expected_names <- c("id", "date_of_onset", "date_of_report", "gender", "outcome")
+  expect_equal(nrow(x), 1L)
+
+  x <- sim_linelist(99, cfr = 0.5)
+  expect_equal(nrow(x), 99L)
+  
+  expect_equal(names(x), expected_names)
+  expect_s3_class(x, "data.frame")
+  expect_s3_class(x$date_of_onset, "Date")
+  expect_s3_class(x$date_of_report, "Date")
+  expect_true(all(x$gender %in% c("female", "male")))
+  expect_true(all(x$outcome %in% c("recovery", "death")))
+
+})
